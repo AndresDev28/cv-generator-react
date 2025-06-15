f0e146c703f28150816b3617df8c1ee8106a910a
AndresDev28 <andresdev28@hotmail.com>
2025-06-15 17:51:20 +0200

    feat: implement form and display modes for general info section

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,9 +1,14 @@
-export default function GeneralInfo({data, onDataChange}) {
+export default function GeneralInfo({data, onDataChange, isEditing, onSubmit, onEdit}) {
 
   return (
     <section>
       <h2>General Information</h2>
-      <form>
+      {isEditing ? (
+        // Si isEditing es true, renderiza el formulario
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}>
         <div>
           <label htmlFor="name">Name</label>
           <input  
@@ -35,6 +40,19 @@ export default function GeneralInfo({data, onDataChange}) {
           <button type="submit">Submit</button>
         </div>
       </form>
+      ) : (
+        // Si isEditing es false, renderiza la VISTA PREVIA DE LA PRESENTACÍON
+        <div>
+          <p><strong>Name:</strong> {data.name}</p>
+          <p><strong>Email:</strong> {data.email}</p>
+          <p><strong>Phone:</strong> {data.phone}</p>
+          <button 
+            type="button"
+            onClick={() => onEdit()}
+            >Edit
+          </button>
+        </div>
+      )}
     </section>  
       
   )
